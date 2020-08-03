8880fde722987da6833c5b3324bc15782482d77e
irrationnelle <drakkarverenis@gmail.com>
2020-08-04 00:51:55 +0900

    feat: implement basic project structure

---
+++ playwright-mocha-playground/test/blog-visit.spec.ts
@@ -0,0 +1,26 @@
+import * as playwright from "playwright";
+import assert from "assert";
+
+let page: any;
+let browser: any;
+let context: any;
+
+const BASE_URL = "https://dev.rase.blog";
+
+describe("개발 블로그에 들어가면", () => {
+    beforeEach(async () => {
+        const { chromium } = playwright;
+        browser = await chromium.launch({ headless: false });
+        context = await browser.newContext();
+        page = await context.newPage(BASE_URL);
+    });
+
+    afterEach(async function() {
+        await browser.close();
+    });
+
+    it("페이지 타이틀로 irrationnelle 을 확인할 수 있다.", async () => {
+        await page.goto("https://dev.rase.blog/");
+        assert.equal(await page.title(), "Home | irrationnelle");
+    });
+});
