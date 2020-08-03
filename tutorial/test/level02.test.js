d3b3c33de34206093ebb29b3e31f9a311355ab0a
MoonSupport <jiwon3346@naver.com>
2020-08-03 23:41:15 +0900

    테스트 추가

--- tutorial/test/level02.test.js
+++ tutorial/test/level02.test.js
@@ -1,6 +1,8 @@
 var assert = require('assert')
 var {sum} = require('../src/level02')
 describe(('Test') , function() {
+    it('get value ',()=> {
+    })
     it('pop & get last element ',()=> {
     })
     it('check arr length ',()=> {
