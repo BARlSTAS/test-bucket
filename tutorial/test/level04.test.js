f268ad1d138270988ab4ffc704681a5eee6d91fe
MoonSupport <jiwon3346@naver.com>
2020-08-04 23:51:35 +0900

    정답 주석

--- tutorial/test/level04.test.js
+++ tutorial/test/level04.test.js
@@ -7,21 +7,21 @@ const email = name+'@email.com'
 describe(('Test') , function() {
     let db
     before(async ()=> {
-        db = await initDB(path.resolve(__dirname, 'test.db'))
-        await dropTable(db)
-        await createTable(db)
+        // db = await initDB(path.resolve(__dirname, 'test.db'))
+        // await dropTable(db)
+        // await createTable(db)
     })
 
     describe('insert students', async ()=> {
        it("학생 테스트" ,async ()=> {
-        await insertStudent(db,name,email)
-        const student = await getStudentsByName(db,name)
-        assert.equal(student[0].name,name)
-        assert.equal(student[0].email,email)
+        // await insertStudent(db,name,email)
+        // const student = await getStudentsByName(db,name)
+        // assert.equal(student[0].name,name)
+        // assert.equal(student[0].email,email)
        })
     })
 
     after(async ()=> {
-        await closeDBPool(db)
+        // await closeDBPool(db)
     })
 })
