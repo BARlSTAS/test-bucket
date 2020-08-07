4ba6014fef46a1c62c4596b286cce83de75ab52c
irrationnelle <drakkarverenis@gmail.com>
2020-08-07 23:54:33 +0900

    feat: solve problem1

--- tutorial/test/level01.test.js
+++ tutorial/test/level01.test.js
@@ -1,6 +1,14 @@
 var assert = require('assert')
 var {sum} = require('../src/level01')
-describe(('Test') , function() {
-    it('test sum',()=> {
+
+describe(('더하기 함수를 사용하면') , function() {
+    it('1과 2를 매개변수로 전달하면 3을 반환한다.', ()=> {
+        var result = sum(1,2);
+        assert(result, 3);
+    })
+
+    it('1과 2를 매개변수로 전달하면 4는 반환하지 않는다.', () => {
+        var result = sum(1,2);
+        assert.notEqual(result, 4);
     })
 })
