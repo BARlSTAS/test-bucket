4565bdf7df33c87b8faf6281988f191ed6d35950
irrationnelle <drakkarverenis@gmail.com>
2020-08-10 23:29:32 +0900

    feat: add birthday spec

---
+++ hackerrank-ts/test/birthday-chocalate.spec.ts
@@ -0,0 +1,9 @@
+//https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+import { expect } from "chai";
+
+describe("생일 초콜릿 문제에서는", () => {
+    it("무엇이든 잘 된다.", () => {
+        console.log("hello world!");
+    });
+});
